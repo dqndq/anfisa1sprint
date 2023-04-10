56461873ee83f872597c8d3d024fe47932c1bf47
Илья Жулихин <iliyazhulihin@mail.ru>
2023-04-10 21:21:34 +0400

    finished project

--- anfisa_for_friends/anfisa_for_friends/settings.py
+++ anfisa_for_friends/anfisa_for_friends/settings.py
@@ -90,7 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'html_templates',
+    BASE_DIR / 'static_dev',
     ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
